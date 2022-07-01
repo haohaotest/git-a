533f1894745150990f7562c1017fe32989a1b684
CHotk <aoptest001@gmail.com>
2022-07-02 05:55:57 +0800

    first commit ~~

---
+++ git_student/src/main/java/com/hao/ABC.java
@@ -0,0 +1,11 @@
+package com.hao;
+
+/**
+ * @outhor Chotck
+ * @create 2022-0702上午 05:53
+ */
+public class ABC {
+    public static void main(String[] args) {
+        System.out.println("Test Message");
+    }
+}
